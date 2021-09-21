4ad9002a11e100f7a41be780dc183b706f2d7784
Emine Bozdag <34144604+eminebozdag@users.noreply.github.com>
2021-09-21 14:13:38 +0300

    Update app.js comment line

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { getData } from "./lib/service.js";
 
-//this function catches and shows the data returned from getdata
+//this function catches and shows the data returned from imported getData function
 (async () => {
   const [{ data: user }, { data: post }] = await getData(2);
   console.log("user: ", user, "post: ", post);
